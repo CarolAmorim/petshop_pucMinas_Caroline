45a6b6be1dcf033176a9cec57ad7bf17fcfb1e66
Caroline Amorim <caroline.lima02@hotmail.com>
2021-10-25 20:39:52 -0300

    Revert "aula 8 - erro"
    This reverts commit 2eef73177823622631cc85f59bb4e699c8e13f38.

--- src/main/java/com/caroline/petshop/domain/Categoria.java
+++ src/main/java/com/caroline/petshop/domain/Categoria.java
@@ -33,13 +33,14 @@ public class Categoria implements Serializable {
 		this.setNome(nome);
 	}
 
-
-
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
 
@@ -56,6 +57,11 @@ public class Categoria implements Serializable {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
 			return false;
 		return true;
 	}
